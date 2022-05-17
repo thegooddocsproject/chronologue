079a6a11fb1f916de2b8f5f6b4cccfb511eda6ca
Ian Nguyen <ian@heythereian.com>
2022-05-16 22:04:01 -0700

    added some api fetch on pages to demo api data

--- pages/ApiCall.jsx
+++ pages/ApiCall.jsx
@@ -1,7 +1,35 @@
+export async function getStaticProps() {
+    // Call an external API endpoint to get posts.
+    // You can use any data fetching library
+    const res = await fetch('https://chronologue.netlify.app/api/server')
+    const posts = await res.json()
+  
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+      props: {
+        posts,
+      },
+    }
+  }
 
-
-export default function ApiCall() {
+export default function ApiCall({posts}) {
+    console.log(posts)
   return (
-    <div>ApiCall</div>
+    <div>
+        
+        <ul >
+      {posts.map((post) => (
+          <>
+        <li key={Math.random()}>{post.years}</li>
+        <li>{post.locations}</li>
+        <li>{post.locationsAndYears}</li>
+        </>
+        ))}
+        </ul>
+      
+    <a href="/">home</a>
+
+    </div>
   )
 }
