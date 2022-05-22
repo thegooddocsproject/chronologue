38f7ebc0db92ab5b2458362527585adf42035243
Ian <ian@heythereian.com>
2022-05-22 05:15:20 +0000

    removed redundant comments

--- pages/ApiCall.jsx
+++ pages/ApiCall.jsx
@@ -1,11 +1,7 @@
 export async function getStaticProps() {
-    // Call an external API endpoint to get posts.
-    // You can use any data fetching library
     const res = await fetch('https://chronologue.netlify.app/api/server')
     const posts = await res.json()
-  
-    // By returning { props: { posts } }, the Blog component
-    // will receive `posts` as a prop at build time
+
     return {
       props: {
         posts,
