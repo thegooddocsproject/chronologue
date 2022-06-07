77dc9ebf78a40a7c795a9e405549065fedf073dd
Ian Nguyen <ian@heythereian.com>
2022-06-06 19:54:15 -0700

    added Community page

--- pages/components/Banner.jsx
+++ pages/components/Banner.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import style from './../../styles/Banner.module.css'
+import React from "react";
+import style from "./../../styles/Banner.module.css";
 
 export default function Banner() {
   return (
     <div className={style.banner}>
-        <p>
-          The Chronologue project is still in development. Join us and develop
-          the telescope of the future!
-        </p>
-      </div>
-  )
+      <p>
+        The Chronologue project is a sample, completely fictional open source
+        project used to create examples of typical open source project
+        documentation. For more information, or to contribute, join the Good
+        Docs community!
+      </p>
+    </div>
+  );
 }
